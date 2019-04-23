9d66a55644b4d20772b0b9eb583713991169f226
fog3211 <384977954@qq.com>
2019-04-23 15:42:52 +0800

    增加卫星地图

--- src/pages/ui/map/index.jsx
+++ src/pages/ui/map/index.jsx
@@ -4,31 +4,39 @@ import ReactQMap from 'react-qmap';
 
 let classMap, windowMap;
 export default class Map extends Component {
-    state={
-        apiKey:"UN6BZ-MP2W6-XWCSX-M2ATU-QORGZ-OWFOE",
-        center:{
-            latitude:36.003147,
-            longitude: 120.12221
+    state = {
+        apiKey: 'UN6BZ-MP2W6-XWCSX-M2ATU-QORGZ-OWFOE',
+        center: {
+            latitude: 36.003147,
+            longitude: 120.12221,
         },
-    }
+        initialOptions: {
+            zoomControl: true,
+            mapTypeControl: true,
+        },
+    };
     setMarker = () => {
-        const {latitude,longitude}=this.state.center;
+        const { latitude, longitude } = this.state.center;
         new windowMap.Marker({
-             map: classMap,
-             position: new windowMap.LatLng(latitude, longitude),
-             animation: windowMap.MarkerAnimation.DROP,
-           });
-         }
+            map: classMap,
+            position: new windowMap.LatLng(latitude, longitude),
+            animation: windowMap.MarkerAnimation.DROP,
+        });
+    };
     getMap = (map, wMap) => {
         classMap = map;
         windowMap = wMap;
         this.setMarker();
-    }
+    };
     render() {
         return (
             <Row gutter={16}>
                 <Col md={24}>
-                     <ReactQMap {...this.state} style={{height: 700}} getMap={(map, wMap) => this.getMap(map, wMap)} />
+                    <ReactQMap
+                        {...this.state}
+                        style={{ height: 700 }}
+                        getMap={(map, wMap) => this.getMap(map, wMap)}
+                    />
                 </Col>
             </Row>
         );
