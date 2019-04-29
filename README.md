00f83b0fd580e9a30f2f124c1c8532fe9127c5dd
fog3211 <384977954@qq.com>
2019-04-29 16:34:05 +0800

    打包配置

--- README.md
+++ README.md
@@ -1,47 +1,56 @@
+
 # react-admin
+
 react后台管理系统，开发中ing
 
 ## 目录结构  
+
 ## 相关依赖
+
 [animate.css](https://github.com/daneden/animate.css) : &nbsp;css动画库  
 [antd](https://ant.design/index-cn) : &nbsp;蚂蚁金服ui组件库  
 [axios](https://github.com/axios/axios) : &nbsp;http请求模块  
 [babel-plugin-import](https://github.com/ant-design/babel-plugin-import) : &nbsp;按需引入组件，优化前端性能  
 [classnames](https://github.com/JedWatson/classnames) : &nbsp;简化React类名写法  
-[d3-scale](https://github.com/d3/d3-scale) : &nbsp;recharts的插件    
-[d3-scale-chromatic](https://github.com/d3/d3-scale-chromatic) : &nbsp;recharts的插件      
-[draft-js](https://github.com/facebook/draft-js) : &nbsp;富文本编辑器    
-[draftjs-to-html](https://github.com/jpuri/draftjs-to-html) : &nbsp;富文本转Html      
-[draftjs-to-markdown](https://github.com/jpuri/draftjs-to-markdown) : &nbsp;富文本转MarkDown     
-[echarts](https://github.com/apache/incubator-echarts) : &nbsp;React的图表插件       
-[echarts-for-react](https://github.com/hustcc/echarts-for-react) : &nbsp;React封装的echart插件       
-[less](https://github.com/less/less.js) : &nbsp;css预处理器       
+[d3-scale](https://github.com/d3/d3-scale) : &nbsp;recharts的插件  
+[d3-scale-chromatic](https://github.com/d3/d3-scale-chromatic) : &nbsp;recharts的插件  
+[draft-js](https://github.com/facebook/draft-js) : &nbsp;富文本编辑器  
+[draftjs-to-html](https://github.com/jpuri/draftjs-to-html) : &nbsp;富文本转Html  
+[draftjs-to-markdown](https://github.com/jpuri/draftjs-to-markdown) : &nbsp;富文本转MarkDown  
+[echarts](https://github.com/apache/incubator-echarts) : &nbsp;React的图表插件  
+[echarts-for-react](https://github.com/hustcc/echarts-for-react) : &nbsp;React封装的echart插件  
+[less](https://github.com/less/less.js) : &nbsp;css预处理器  
 [mockjs](http://mockjs.com/) : &nbsp;模拟生成数据，拦截登录等  
-[nprogress](https://github.com/rstacruz/nprogress) : &nbsp;加载进度条    
-[photoswipe](https://github.com/dimsemenov/photoswipe) : &nbsp;图片弹层查看插件 
-[prop-types](https://github.com/facebook/prop-types) : &nbsp;props属性类型检查    
-[react-color](https://github.com/casesandberg/react-color) : &nbsp;调色板组件   
-[react-draft-wysiwyg](https://github.com/jpuri/react-draft-wysiwyg) : &nbsp;富文本组件   
-[react-draggable](https://github.com/mzabriskie/react-draggable) : &nbsp;React拖拽插件     
-[react-highlight-words](https://github.com/bvaughn/react-highlight-words) : &nbsp;table表格字体高亮    
+[photoswipe](https://github.com/dimsemenov/photoswipe) : &nbsp;图片弹层查看插件  
+[prop-types](https://github.com/facebook/prop-types) : &nbsp;props属性类型检查  
+[react-color](https://github.com/casesandberg/react-color) : &nbsp;调色板组件  
+[react-draft-wysiwyg](https://github.com/jpuri/react-draft-wysiwyg) : &nbsp;富文本组件  
+[react-draggable](https://github.com/mzabriskie/react-draggable) : &nbsp;React拖拽插件  
+[react-highlight-words](https://github.com/bvaughn/react-highlight-words) : &nbsp;table表格字体高亮  
 [react-lottie](https://github.com/chenqingspring/react-lottie) : &nbsp;React版Lottie动画  
 [react-qmap](https://github.com/yezihaohao/react-qmap) : &nbsp;React版地图组件  
 [recharts](https://github.com/recharts/recharts) : &nbsp;另一个React的图表插件  
 [screenfull](https://github.com/sindresorhus/screenfull.js/) : &nbsp;全屏插件  
 
-## 功能实现 
+## 功能实现
 
-## 功能展示 
+## 功能展示
 
 ## 技术障碍
+
 ### 主观障碍
-图表功能中部分文字超出隐藏（echart本身生成canvas时的bug）    
-日历坐标系在移动端显示太过密集（echart部分组件没有实现响应式）   
+
+图表功能中部分文字超出隐藏（echart本身生成canvas时的bug）  
+日历坐标系在移动端显示太过密集（echart部分组件没有实现响应式）  
 旭日图偶尔会出现宽度为100px不变的情况（疑似echart的bug）
+
 ### 客观障碍
+
 富文本功能无法上传预览图片（[imgur](https://api.imgur.com/3/image)接口不可用）  
-旭日图图表子节点太复杂无法模拟生成数据 
+旭日图图表子节点太复杂无法模拟生成数据  
+
 ## 感谢
+
 蚂蚁金服的组件库[Ant Design](https://ant.design/index-cn)  
 yezihaohao开源的[react-admin](https://github.com/yezihaohao/react-admin)项目  
 Ciwing·Jon提供的[漫威头像图标](https://www.iconfont.cn/collections/detail?cid=15515)
---
+++ build/precache-manifest.02d4a32a860d8df58440a209dc76db61.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98db5613cc8f3690c55",
+    "url": "./static/css/main.7c58a9ef.chunk.css"
+  },
+  {
+    "revision": "f98db5613cc8f3690c55",
+    "url": "./static/js/main.8467ef22.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "63cb91b347f0d593b33c",
+    "url": "./static/css/2.f9bad28c.chunk.css"
+  },
+  {
+    "revision": "63cb91b347f0d593b33c",
+    "url": "./static/js/2.7d3d2509.chunk.js"
+  },
+  {
+    "revision": "00bb3ded380529935cc4d5d634c88b51",
+    "url": "./static/media/head.00bb3ded.png"
+  },
+  {
+    "revision": "e655f03eb23aae5ef94ba42dfee8fbf2",
+    "url": "./static/media/logo.e655f03e.svg"
+  },
+  {
+    "revision": "e147bdebf32ef9cb7ce137ed0ed5d3a6",
+    "url": "./static/media/1.e147bdeb.png"
+  },
+  {
+    "revision": "53c840d876ce5a1077fa115bef0331ef",
+    "url": "./static/media/2.53c840d8.png"
+  },
+  {
+    "revision": "426a2ffe6e8d2dd0335e218c77bdeb1e",
+    "url": "./static/media/3.426a2ffe.png"
+  },
+  {
+    "revision": "473a3c8731a5213caf1b877f805085c4",
+    "url": "./static/media/4.473a3c87.png"
+  },
+  {
+    "revision": "4e81901bc88de12fb6465d0fd5858235",
+    "url": "./static/media/5.4e81901b.png"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "7df2d8c05d09f4b22b8fc4b580191eb5",
+    "url": "./static/media/1.7df2d8c0.png"
+  },
+  {
+    "revision": "fbc868eb6e8b88a8028adafb3651042a",
+    "url": "./static/media/2.fbc868eb.png"
+  },
+  {
+    "revision": "e44f526a947589f6f3c3881671fe0015",
+    "url": "./static/media/3.e44f526a.png"
+  },
+  {
+    "revision": "61dd2ea4d3337e5bdb408889355574ad",
+    "url": "./static/media/4.61dd2ea4.png"
+  },
+  {
+    "revision": "9849ca50bf1805a66ced2c00664e50ef",
+    "url": "./static/media/5.9849ca50.png"
+  },
+  {
+    "revision": "7c6b1ee8ede5bd0d145d2b44b345c580",
+    "url": "./static/media/6.7c6b1ee8.png"
+  },
+  {
+    "revision": "1554fd456fb587599dcf319b4ee312d2",
+    "url": "./static/media/7.1554fd45.png"
+  },
+  {
+    "revision": "afe91634a5d23459773bd455a322fc15",
+    "url": "./static/media/8.afe91634.png"
+  },
+  {
+    "revision": "9c4b9dd0255b37c33d1d457f60a9630a",
+    "url": "./static/media/9.9c4b9dd0.png"
+  },
+  {
+    "revision": "3782603c55cafb76aab60342ba18c1ee",
+    "url": "./static/media/10.3782603c.png"
+  },
+  {
+    "revision": "df66f598f5c19ab8316829093b82352c",
+    "url": "./static/media/11.df66f598.png"
+  },
+  {
+    "revision": "74b1624e70864c53a02dc470a20fe2eb",
+    "url": "./static/media/12.74b1624e.png"
+  },
+  {
+    "revision": "3fdd9e8c2ae0618cbdcae2e6dc53e07c",
+    "url": "./static/media/13.3fdd9e8c.png"
+  },
+  {
+    "revision": "2717acfbac8262bdd0a7741e0e897177",
+    "url": "./static/media/14.2717acfb.png"
+  },
+  {
+    "revision": "d139cf7e4be73ba5c42d003958eb6490",
+    "url": "./static/media/15.d139cf7e.png"
+  },
+  {
+    "revision": "00dc66fce634fe9ca4c0b14841a865c4",
+    "url": "./static/media/16.00dc66fc.png"
+  },
+  {
+    "revision": "66c653ce9d500cc1c2af6ab61380c587",
+    "url": "./static/media/17.66c653ce.png"
+  },
+  {
+    "revision": "7c6e4ead97bace96457d9490b03df462",
+    "url": "./static/media/18.7c6e4ead.png"
+  },
+  {
+    "revision": "d3d8cf1d23f714b340feca2d295a409c",
+    "url": "./static/media/19.d3d8cf1d.png"
+  },
+  {
+    "revision": "73152b90d2a5894231959e45855ea749",
+    "url": "./static/media/20.73152b90.png"
+  },
+  {
+    "revision": "503a191a39ed6388f160aa14fa441636",
+    "url": "./static/media/login-bg.503a191a.jpg"
+  },
+  {
+    "revision": "db966eb2ba23fec2ffa6e52de2270a7d",
+    "url": "./index.html"
+  }
+];
